dc27c9209c74e2e94ab0b9c20ece34f06902fc24
Zerdayne <maik@zimmermann.zone>
2022-09-04 08:22:25 +0200

    storage lib

---
+++ lib/storage.lua
@@ -0,0 +1,13 @@
+function init(monitor, inv)
+    local count = 0
+
+    for slot, item in pairs(inv.list()) do
+        count = count + 1
+    end
+
+    monitor.setCursorPos(1, 1)
+    monitor.write("Storage")
+
+    monitor.setCursorPos(1, 2)
+    monitor.write(count .. "/" .. inv.size())
+end
--- startup.lua
+++ startup.lua
@@ -1,20 +1,12 @@
+local storage = require("lib.storage")
+
 local monitor = peripheral.wrap("bottom")
 local inv = peripheral.wrap("toms_storage:ts.inventory_connector.tile_0")
 
 while true do
     monitor.clear()
 
-    local count = 0
-
-    for slot, item in pairs(inv.list()) do
-        count = count + 1
-    end
-
-    monitor.setCursorPos(1, 1)
-    monitor.write("Storage")
-
-    monitor.setCursorPos(1, 2)
-    monitor.write(count .. "/" .. inv.size())
+    storage.init()
 
     sleep(30)
 
