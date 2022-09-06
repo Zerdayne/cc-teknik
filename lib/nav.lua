58882f156236543b09d9345980bbeaa27dab6684
Zerdayne <maik@zimmermann.zone>
2022-09-06 19:10:48 +0200

    changes

--- lib/nav.lua
+++ lib/nav.lua
@@ -7,6 +7,12 @@ function init(monitor)
     monitor.setCursorPos(2, 2)
     monitor.write("Dashboard")
 
+    monitor.setCursorPos(2, 4)
+    monitor.write("Storage  ")
+
+    monitor.setCursorPos(2, 6)
+    monitor.write("Energy   ")
+
     helper.resetColors(monitor)
 end
 
